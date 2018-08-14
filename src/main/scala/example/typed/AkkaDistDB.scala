817fae9ac1ccd34bd4fd229c9fcc5ea54d8c97d3
leena-gokhale <gokhale.leena@gmail.com>
2018-08-14 10:48:30 +0530

    Compliling

--- src/main/scala/example/typed/AkkaDistDB.scala
+++ src/main/scala/example/typed/AkkaDistDB.scala
@@ -1,11 +1,11 @@
 package example.typed
+import akka.Done
+
 import scala.concurrent.Future
 
 trait AkkaDistDB {
-
-  def set(key: String, value: Int)
-  def getAll(): Future[Map[String, Int]]
-  def get(key: String): Future[Map[String, Int]]
-  def remove(key: String)
-
+  def set(key: String, value: Int): Future[Done]
+  def getAll: Future[Map[String, Int]]
+  def get(key: String): Future[Option[Int]]
+  def remove(key: String): Future[Done]
 }
--- src/main/scala/example/typed/AkkaDistImpl.scala
+++ src/main/scala/example/typed/AkkaDistImpl.scala
@@ -1,34 +1,41 @@
 package example.typed
+import akka.Done
 import akka.actor.typed.scaladsl.adapter.TypedActorSystemOps
 import akka.actor.typed.{ActorRef, ActorSystem}
 import akka.cluster.Cluster
-import akka.cluster.ddata.typed.scaladsl.Replicator.Update
-import akka.cluster.ddata.{LWWMap, LWWMapKey, Replicator}
 import akka.cluster.ddata.typed.scaladsl.{DistributedData, Replicator}
+import Replicator.{Update, UpdateResponse}
+import akka.actor.Scheduler
+import akka.cluster.ddata.{LWWMap, LWWMapKey}
 import example.typed.AkkaDB.ActionOnDB
-import example.typed.AkkaDistDB
+import akka.actor.typed.scaladsl.AskPattern._
+import akka.util.Timeout
 
 import scala.concurrent.Future
+import scala.concurrent.duration.DurationInt
 
 class AkkaDistImpl(system: ActorSystem[ActionOnDB]) extends AkkaDistDB {
 
-  val replicator                = DistributedData(system).replicator
-  implicit val cluster: Cluster = akka.cluster.Cluster(system.toUntyped)
+  private val replicator                    = DistributedData(system).replicator
+  private implicit val cluster: Cluster     = akka.cluster.Cluster(system.toUntyped)
+  private implicit val scheduler: Scheduler = system.scheduler
+  import system.executionContext
+  private implicit val timeout: Timeout = Timeout(5.seconds)
 
   //This key should hv some additional variable added to it like say table name that comes from outside this object
-  val DataKey = LWWMapKey[String, Int]("LWWMapKey")
+  val DataKey: LWWMapKey[String, Int] = LWWMapKey[String, Int]("LWWMapKey")
 
-  def set(key: String, value: Int) = {
-    val update = akka.cluster.ddata.Replicator
-      .Update(DataKey, LWWMap.empty[String, Int], akka.cluster.ddata.Replicator.WriteLocal)((_ + (key, value)))
-    //replicator ! update
+  override def set(key: String, value: Int): Future[Done] = {
+    val update: ActorRef[UpdateResponse[LWWMap[String, Int]]] => Update[LWWMap[String, Int]] =
+      Replicator.Update(DataKey, LWWMap.empty[String, Int], Replicator.WriteLocal)(_ + (key, value))
+
+    (replicator ? update).map {
+      case x: Replicator.UpdateSuccess[_] => Done
+      case x                              => throw new RuntimeException(s"update failed due to: $x")
+    }
   }
-
-  def getAll(): Future[Map[String, Int]] = {
-    //val getValues: Map[String, Int] = Map("a"-> 1)
-  }
-
-  def get(key: String): Future[Map[String, Int]] = {}
-  def remove(key: String)                        = {}
+  override def getAll: Future[Map[String, Int]]      = ???
+  override def get(key: String): Future[Option[Int]] = ???
+  override def remove(key: String): Future[Done]     = ???
 
 }
