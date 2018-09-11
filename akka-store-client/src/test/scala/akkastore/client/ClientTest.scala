42d32c05a3f7a2720103dc5d2beceb0c802cdb6d
leena-gokhale <gokhale.leena@gmail.com>
2018-09-11 14:48:23 +0530

    Test with different test models
    - Tested the entire work flow cycle with test models. With keys of different types (Int and String)

--- akka-store-client/src/test/scala/akkastore/client/ClientTest.scala
+++ akka-store-client/src/test/scala/akkastore/client/ClientTest.scala
@@ -10,11 +10,34 @@ class ClientTest extends FunSuite with Matchers with TestJsonSupport {
 
   test("dd") {
     implicit val actorSystem: ActorSystem = ActorSystem("test")
-    val client                            = new AkkaStoreClient[Id, Person]("http://localhost:8080/akkastore")
 
-    client.set(Id("123"), Person("leena", 99)).get
+    //Testing with model that has key of type Int
+    val client = new AkkaStoreClient[NumId, NumStrDatails]("http://localhost:8080/akkastore/demo-store")
 
-    println(client.get(Id("123")).get)
+    client.set(NumId(1), NumStrDatails("name1", "9898989")).get
+    client.set(NumId(2), NumStrDatails("name2", "878787")).get
+    client.set(NumId(3), NumStrDatails("name3", "989890")).get
+    client.set(NumId(4), NumStrDatails("name4", "454545")).get
+    client.set(NumId(5), NumStrDatails("name5", "121121")).get
+
+    println(client.get(NumId(1)).get)
+    client.remove(NumId(2))
+    println((client.list).get)
+
+    Thread.sleep(2000)
+
+    //Testing with model that has key of type String
+
+    val client2 = new AkkaStoreClient[Id, Person]("http://localhost:8080/akkastore/demo-store2")
+
+    client2.set(Id("001"), Person("name1", 10)).get
+    client2.set(Id("002"), Person("name2", 20)).get
+    client2.set(Id("003"), Person("name3", 30)).get
+    client2.set(Id("004"), Person("name4", 40)).get
+    client2.set(Id("005"), Person("name5", 50)).get
+    println(client2.get(Id("001")).get)
+    client2.remove(Id("002"))
+    println((client2.list).get)
 
   }
 
--- akka-store-client/src/test/scala/akkastore/client/testmodels.scala
+++ akka-store-client/src/test/scala/akkastore/client/testmodels.scala
@@ -4,7 +4,13 @@ import play.api.libs.json.{Format, Json}
 case class Id(value: String)
 case class Person(name: String, age: Int)
 
+case class NumId(Value: Int)
+case class NumStrDatails(name: String, phone: String)
+
 trait TestJsonSupport {
   implicit val idFormat: Format[Id]         = Json.format[Id]
   implicit val personFormat: Format[Person] = Json.format[Person]
+
+  implicit val numFormat: Format[NumId]                   = Json.format[NumId]
+  implicit val numStrDatailsFormat: Format[NumStrDatails] = Json.format[NumStrDatails]
 }
