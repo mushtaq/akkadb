4989085132b828d0f591bc4f0e9896d1a4391412
leena-gokhale <gokhale.leena@gmail.com>
2018-09-12 08:44:39 +0530

    Akka http server host and port details from application.conf file.
    It was hard coded before

--- akka-store-server/src/main/scala/akkastore/server/AkkaStoreServer.scala
+++ akka-store-server/src/main/scala/akkastore/server/AkkaStoreServer.scala
@@ -1,13 +1,19 @@
 package akkastore.server
 
+import com.typesafe.config._
 import akka.http.scaladsl.Http
 
 import scala.concurrent.Future
 
 class AkkaStoreServer(route: AkkaStoreRoutes, actorRuntime: ActorRuntime) {
   import actorRuntime._
+
+  val config          = ConfigFactory.load()
+  val httpPserverPort = config.getInt("akka.http-port")
+  val httphost        = config.getString("akka.http-host")
+
   def start(): Future[Http.ServerBinding] = {
-    println("server started don't know the port *** ")
-    Http().bindAndHandle(route.route, "localhost", 8080)
+    println("Akka http server started at  *** " + httphost + ":" + httpPserverPort)
+    Http().bindAndHandle(route.route, httphost, httpPserverPort)
   }
 }
